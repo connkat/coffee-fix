47db82d95d1815b05d6736629895a0e083ec48b7
connkat <hi.connkat@gmail.com>
2023-05-04 23:55:06 -0700

    added tooltip

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "../App.css";
+
+type Props = {
+	tip: string;
+};
+
+function Tooltip({ tip }: Props) {
+
+	return (
+		<div className="Tooltip">
+			
+		</div>
+	);
+}
+
+export default Tooltip;
