eccf582b67999793195116a2ffebccf06b0d9a8a
Kat Connolly <hi.connkat@gmail.com>
2023-10-31 23:31:39 -0600

    Unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 import "./App.css";
 
-import FormGroup from "@mui/material/FormGroup";
+{/* import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import Checkbox from "@mui/material/Checkbox"; */}
 import CoffeeForm from "./components/CoffeeForm";
 
 function App() {
