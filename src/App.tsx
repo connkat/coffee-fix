e1d7057f734f73e5b97d6a2f96997a8680815374
connkat <hi.connkat@gmail.com>
2023-11-15 23:28:27 -0700

    tongue chart

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 
 import "./App.css";
 
+<<<<<<< Updated upstream
 {/* import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox"; */}
+=======
+// import FormGroup from "@mui/material/FormGroup";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+>>>>>>> Stashed changes
 import CoffeeForm from "./components/CoffeeForm";
 
 function App() {
 	// const [temp, setTemp] = useState(false);
+	const [tongue, setTongue] = useState(false);
+
+	const onTongueClick = () => {
+		setTongue(!tongue);
+	};
 
 	return (
 		<div className="App">
@@ -27,6 +38,10 @@ function App() {
 				</FormGroup> */}
 			</div>
 			<CoffeeForm />
+			{/* <div className="tongue-chart">
+				Not sure how to tell the difference between bitter and sour taste? Check out <button onClick={onTongueClick}>this chart</button>!
+				<img className="tongue-img" src={} alt="tongue chart" />
+			</div> */}
 		</div>
 	);
 }
