fbc0ee56d95e7ef2037151af64a5f327acdd897d
connkat <hi.connkat@gmail.com>
2023-11-26 16:28:03 -0700

    set props

--- src/components/CoffeeForm/SelectMenu.tsx
+++ src/components/CoffeeForm/SelectMenu.tsx
@@ -7,13 +7,15 @@ export default function SelectMenu(props: SelectProps) {
 	return (
 		<div className="SelectMenu">
 			<select onChange={props.onChange}>
-				<option hidden key="default" value="select">
-					Select
-				</option>
 				{props.options.map((option, index) => (
-					<option key={index} value={option}>
-						{option}
-					</option>
+					<>
+						<option hidden key="default" value="select">
+							Select
+						</option>
+						<option key={index} value={option}>
+							{option}
+						</option>
+					</>
 				))}
 			</select>
 		</div>
--- src/lib/helperFunctions.ts
+++ src/lib/helperFunctions.ts
@@ -8,30 +8,102 @@ const options3 = ["Non existent"];
 const options4 = ["Bitter", "Sour"];
 
 const solutions = [
-	{ id: 1, tip: "Not enough coffee", additionalInfo: "info" },
-	{ id: 2, tip: "Your grind is too coarse", additionalInfo: "info" },
-	{ id: 3, tip: "Your tamp is too light", additionalInfo: "info" },
-	{ id: 4, tip: "Your temperature is too hot", additionalInfo: "info" },
-	{ id: 5, tip: "Too much coffee", additionalInfo: "info" },
-	{ id: 6, tip: "Your grind is too fine", additionalInfo: "info" },
-	{ id: 7, tip: "Your tamp is too hard", additionalInfo: "info" },
+	{
+		id: 1,
+		tip: "Not enough coffee",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 2,
+		tip: "Your grind is too coarse",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 3,
+		tip: "Your tamp is too light",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 4,
+		tip: "Your temperature is too hot",
+		additionalInfo: "explanation coming soon!",
+	},
+	{ id: 5, tip: "Too much coffee", additionalInfo: "explanation coming soon!" },
+	{
+		id: 6,
+		tip: "Your grind is too fine",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 7,
+		tip: "Your tamp is too hard",
+		additionalInfo: "explanation coming soon!",
+	},
 	{
 		id: 8,
 		tip: "You can temperature is too low",
-		additionalInfo: "info",
+		additionalInfo: "explanation coming soon!",
 	},
-	{ id: 9, tip: "Not enough coffee", additionalInfo: "info" },
-	{ id: 10, tip: "Your grind is too coarse", additionalInfo: "info" },
-	{ id: 11, tip: "Your tamp is too light", additionalInfo: "info" },
-	{ id: 12, tip: "Your temperature is too hot", additionalInfo: "info" },
-	{ id: 13, tip: "Too much coffee", additionalInfo: "info" },
-	{ id: 14, tip: "Your grind is too fine", additionalInfo: "info" },
-	{ id: 15, tip: "Your tamp is too hard", additionalInfo: "info" },
-	{ id: 16, tip: "Your temperature is too low", additionalInfo: "info" },
-	{ id: 17, tip: "Your grind is too fine", additionalInfo: "info" },
-	{ id: 18, tip: "Your tamp is too hard.", additionalInfo: "info" },
-	{ id: 19, tip: "Your grind is too coarse", additionalInfo: "info" },
-	{ id: 20, tip: "Your tamp is too light", additionalInfo: "info" },
+	{
+		id: 9,
+		tip: "Not enough coffee",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 10,
+		tip: "Your grind is too coarse",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 11,
+		tip: "Your tamp is too light",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 12,
+		tip: "Your temperature is too hot",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 13,
+		tip: "Too much coffee",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 14,
+		tip: "Your grind is too fine",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 15,
+		tip: "Your tamp is too hard",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 16,
+		tip: "Your temperature is too low",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 17,
+		tip: "Your grind is too fine",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 18,
+		tip: "Your tamp is too hard.",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 19,
+		tip: "Your grind is too coarse",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 20,
+		tip: "Your tamp is too light",
+		additionalInfo: "explanation coming soon!",
+	},
 	{
 		id: 21,
 		tip: "Your grind is the wrong size",
@@ -46,13 +118,25 @@ const solutions = [
 			"Stale coffee beans have been exposed to oxygen, thus breaking down the oils that give high-quality coffee its flavor.",
 		link: "https://www.businessinsider.com/why-coffee-gets-stale-when-sitting-out-oxygenation-2015-9?r=MX&IR=T",
 	},
-	{ id: 23, tip: "Your grind is too fine", additionalInfo: "info" },
-	{ id: 24, tip: "You are using too much coffee", additionalInfo: "info" },
-	{ id: 25, tip: "Your grind is too coarse", additionalInfo: "info" },
+	{
+		id: 23,
+		tip: "Your grind is too fine",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 24,
+		tip: "You are using too much coffee",
+		additionalInfo: "explanation coming soon!",
+	},
+	{
+		id: 25,
+		tip: "Your grind is too coarse",
+		additionalInfo: "explanation coming soon!",
+	},
 	{
 		id: 26,
 		tip: "You are not using enough coffee",
-		additionalInfo: "info",
+		additionalInfo: "explanation coming soon!",
 	},
 ];
 
@@ -80,45 +164,121 @@ export function findSolutions(
 	let output = obj;
 	if (description === "Pour Time" && option === "Too fast") {
 		return [
-			{ id: 1, tip: "Not enough coffee", additionalInfo: "info" },
-			{ id: 2, tip: "Your grind is too coarse", additionalInfo: "info" },
-			{ id: 3, tip: "Your tamp is too light", additionalInfo: "info" },
-			{ id: 4, tip: "Your temperature is too hot", additionalInfo: "info" },
+			{
+				id: 1,
+				tip: "Not enough coffee",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 2,
+				tip: "Your grind is too coarse",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 3,
+				tip: "Your tamp is too light",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 4,
+				tip: "Your temperature is too hot",
+				additionalInfo: "explanation coming soon!",
+			},
 		];
 	} else if (description === "Pour Time" && option === "Too slow") {
 		return [
-			{ id: 5, tip: "Too much coffee", additionalInfo: "info" },
-			{ id: 6, tip: "Your grind is too fine", additionalInfo: "info" },
-			{ id: 7, tip: "Your tamp is too hard", additionalInfo: "info" },
+			{
+				id: 5,
+				tip: "Too much coffee",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 6,
+				tip: "Your grind is too fine",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 7,
+				tip: "Your tamp is too hard",
+				additionalInfo: "explanation coming soon!",
+			},
 			{
 				id: 8,
 				tip: "You can temperature is too low",
-				additionalInfo: "info",
+				additionalInfo: "explanation coming soon!",
 			},
 		];
 	} else if (description === "Pour Time" && option === "Too fast") {
 		return [
-			{ id: 9, tip: "Not enough coffee", additionalInfo: "info" },
-			{ id: 10, tip: "Your grind is too coarse", additionalInfo: "info" },
-			{ id: 11, tip: "Your tamp is too light", additionalInfo: "info" },
-			{ id: 12, tip: "Your temperature is too hot", additionalInfo: "info" },
+			{
+				id: 9,
+				tip: "Not enough coffee",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 10,
+				tip: "Your grind is too coarse",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 11,
+				tip: "Your tamp is too light",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 12,
+				tip: "Your temperature is too hot",
+				additionalInfo: "explanation coming soon!",
+			},
 		];
 	} else if (description === "Pour Time" && option === "Too slow") {
 		output = [
-			{ id: 13, tip: "Too much coffee", additionalInfo: "info" },
-			{ id: 14, tip: "Your grind is too fine", additionalInfo: "info" },
-			{ id: 15, tip: "Your tamp is too hard", additionalInfo: "info" },
-			{ id: 16, tip: "Your temperature is too low", additionalInfo: "info" },
+			{
+				id: 13,
+				tip: "Too much coffee",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 14,
+				tip: "Your grind is too fine",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 15,
+				tip: "Your tamp is too hard",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 16,
+				tip: "Your temperature is too low",
+				additionalInfo: "explanation coming soon!",
+			},
 		];
 	} else if (description === "Pressure" && option === "Too high") {
 		output = [
-			{ id: 17, tip: "Your grind is too fine", additionalInfo: "info" },
-			{ id: 18, tip: "Your tamp is too hard.", additionalInfo: "info" },
+			{
+				id: 17,
+				tip: "Your grind is too fine",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 18,
+				tip: "Your tamp is too hard.",
+				additionalInfo: "explanation coming soon!",
+			},
 		];
 	} else if (description === "Pressure" && option === "Too low") {
 		output = [
-			{ id: 19, tip: "Your grind is too coarse", additionalInfo: "info" },
-			{ id: 20, tip: "Your tamp is too light", additionalInfo: "info" },
+			{
+				id: 19,
+				tip: "Your grind is too coarse",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 20,
+				tip: "Your tamp is too light",
+				additionalInfo: "explanation coming soon!",
+			},
 		];
 	} else if (description === "Crema") {
 		output = [
@@ -139,16 +299,28 @@ export function findSolutions(
 		];
 	} else if (description === "Taste" && option === "Bitter") {
 		output = [
-			{ id: 23, tip: "Your grind is too fine", additionalInfo: "info" },
-			{ id: 24, tip: "You are using too much coffee", additionalInfo: "info" },
+			{
+				id: 23,
+				tip: "Your grind is too fine",
+				additionalInfo: "explanation coming soon!",
+			},
+			{
+				id: 24,
+				tip: "You are using too much coffee",
+				additionalInfo: "explanation coming soon!",
+			},
 		];
 	} else if (description === "Taste" && option === "Sour") {
 		output = [
-			{ id: 25, tip: "Your grind is too coarse", additionalInfo: "info" },
+			{
+				id: 25,
+				tip: "Your grind is too coarse",
+				additionalInfo: "explanation coming soon!",
+			},
 			{
 				id: 26,
 				tip: "You are not using enough coffee",
-				additionalInfo: "info",
+				additionalInfo: "explanation coming soon!",
 			},
 		];
 	} else return [];
